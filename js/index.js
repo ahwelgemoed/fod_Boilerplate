c073ac0a4d09510aa56e6e7a3ab102ea830d72a0
Arno <arno.welgemoed@outlook.com>
2019-08-20 08:22:55 +0200

    🔥

--- js/index.js
+++ js/index.js
@@ -5,7 +5,9 @@ let ccsBox = document.getElementsByClassName('ccsBox');
 let codeBlock1 = document.getElementsByClassName('codeBlock1');
 let codeBlock2 = document.getElementsByClassName('codeBlock2');
 let codeBlock3 = document.getElementsByClassName('codeBlock3');
+
 let tl = new TimelineMax({ onComplete });
+
 tl.from(Desk, 1, { opacity: 0, y: 100 })
   .from(
     codeBlock1,
@@ -49,9 +51,47 @@ tl.from(Desk, 1, { opacity: 0, y: 100 })
   );
 
 function onComplete() {
+  // alert('Now')
   let ccs = new TimelineMax({ repeat: -1 });
   ccs.to(ccsBox, 1, { paddingLeft: 170 }).to(ccsBox, 1, { paddingLeft: 160 });
 
   let js = new TimelineMax({ repeat: -1 });
   js.to(jsBox, 1, { paddingLeft: 10 }).to(jsBox, 0.5, { paddingLeft: 0 });
 }
+
+tl.pause();
+
+var play = document.getElementById('play');
+var pause = document.getElementById('pause');
+var resume = document.getElementById('resume');
+var speedUp = document.getElementById('speedUp');
+var slowDown = document.getElementById('slowDown');
+var seek = document.getElementById('seek');
+var progress = document.getElementById('progress');
+
+play.onclick = function() {
+  tl.play();
+};
+pause.onclick = function() {
+  tl.pause();
+};
+restart.onclick = function() {
+  tl.restart();
+};
+resume.onclick = function() {
+  tl.resume();
+};
+speedUp.onclick = function() {
+  tl.timeScale(5);
+};
+slowDown.onclick = function() {
+  tl.timeScale(0.5);
+};
+seek.onclick = function() {
+  tl.seek(2);
+  // Amount Of SEC
+};
+progress.onclick = function() {
+  tl.progress(0.5);
+  // Amount Of SEC
+};

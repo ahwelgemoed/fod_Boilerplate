5d3602830a0905045d428be7e867b7d73b26685a
Arno <arno.welgemoed@outlook.com>
2019-08-22 11:05:03 +0200

    👨🏼‍🚒

--- js/index.js
+++ js/index.js
@@ -3,55 +3,12 @@ import '../css/main.scss';
 var mainBody = document.getElementById('mainBody');
 var dotsArray = document.getElementById('dotsArray');
 var runTime = document.getElementById('runTime');
-var yello2svg = document.getElementById('yello2svg');
+var yellow2svg = document.getElementById('yello2svg');
 var white1svg = document.getElementById('white1svg');
-var i = 1;
+
 var pageOpenTimeline = new TimelineMax();
 var chnageBackGroundColor = new TimelineMax();
-// window.innerWidth
-var white1svgPath = {
-  // curviness: 1.5,
-  // autoRotate: true,
-  // type: 'cubic',
-  // type: 'thru',
-  values: [
-    { left: '10%', bottom: '15%', rotation: 0 },
-    { left: '15%', bottom: '25%', rotation: 90 },
-    { bottom: '55%', left: '15%', rotation: 180 },
-    { bottom: '55%', left: '40%', rotation: 180 },
-    { bottom: '40%', left: '40%', rotation: 180 }
-  ]
-};
 
 pageOpenTimeline
-  .set(dotsArray, { top: '-50%', left: '-10%' })
   .from(dotsArray, 2, { top: '-60%' })
-  .from(yello2svg, 2, { right: '-5%', opacity: 0 }, '-=1.5')
-  .to(
-    white1svg,
-    3,
-    { bezier: white1svgPath, ease: Power4.easeOut, opacity: 0.5 },
-    '-=2.5'
-  );
-
-let controller = new ScrollMagic.Controller();
-var sceneTOne = new ScrollMagic.Scene({
-  triggerElement: '#trigger'
-  // duration: '30%'
-  // triggerHook: 0
-})
-  .setTween(pageOpenTimeline)
-  .addIndicators()
-  .addTo(controller);
-
-// chnageBackGroundColor.to(mainBody, 0.5, { backgroundColor: '#cc2525' });
-// chnageBackGroundColor.pause();
-// function onRepeat() {
-//   i++;
-//   runTime.innerHTML = 'I have run' + ' ' + i + ' ' + 'times';
-//   if (i % 5 === 0) {
-//     chnageBackGroundColor.play();
-//   } else {
-//     chnageBackGroundColor.reverse();
-//   }
-// }
+  .from(yellow2svg, 2, { right: '-5%', opacity: 0 }, '-=1.5');

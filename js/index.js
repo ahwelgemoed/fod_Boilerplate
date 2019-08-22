39bb65e6e93229edc4a76f3c24a70ccead847780
Arno <arno.welgemoed@outlook.com>
2019-08-22 15:15:23 +0200

    😃

--- js/index.js
+++ js/index.js
@@ -9,6 +9,26 @@ var white1svg = document.getElementById('white1svg');
 var pageOpenTimeline = new TimelineMax();
 var chnageBackGroundColor = new TimelineMax();
 
+var white1svgOptions = [
+  { left: '10%', bottom: '15%' },
+  { left: '30%', bottom: '40%' },
+  { left: '30%', bottom: '40%' },
+  { left: '80%', bottom: '60%' }
+];
+
 pageOpenTimeline
   .from(dotsArray, 2, { top: '-60%' })
-  .from(yellow2svg, 2, { right: '-5%', opacity: 0 }, '-=1.5');
+  .from(yellow2svg, 2, { right: '-5%', opacity: 0 }, '-=1.5')
+  .to(white1svg, 2, { bezier: white1svgOptions, opacity: 0 }, '-=1.5');
+
+var controller = new ScrollMagic.Controller();
+
+var sceneOne = new ScrollMagic.Scene({
+  triggerElement: '#trigger1',
+  triggerHook: 0,
+  duration: '80%'
+})
+  .setTween(pageOpenTimeline)
+  .addIndicators()
+  .setPin('#trigger1')
+  .addTo(controller);
